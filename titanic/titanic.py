cc68888a87dc922e045daa7922f0a36ebad4f9c1
Kotaro Isobe <kisobe0609@gmail.com>
2023-09-20 21:58:34 +0900

    first submission

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -16,8 +16,25 @@ for dirname, _, filenames in os.walk('input'):
 # Any results you write to the current directory are saved as output.
 
 train_data = pd.read_csv("./input/train.csv")
+test_data = pd.read_csv("./input/test.csv")
 print(train_data)
 
-women = train_data.loc[train_data.Sex == 'female']["Survived"]
-rate_women = sum(women)/len(women)
-print("% of women survived:", rate_women)
+# women = train_data.loc[train_data.Sex == 'female']["Survived"]
+# rate_women = sum(women)/len(women)
+# print("% of women survived:", rate_women)
+
+from sklearn.ensemble import RandomForestClassifier
+
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('submission.csv', index=False)
+print("Your submission was successfully saved!")
